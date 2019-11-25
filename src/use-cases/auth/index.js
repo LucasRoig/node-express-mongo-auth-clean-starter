33b5765affae77ebc48ac7cb7db38709809dba45
Lucas Roig <lucas1.roig@gmail.com>
2019-11-25 20:00:07 +0100

    add jsonwebtoken

--- src/use-cases/auth/index.js
+++ src/use-cases/auth/index.js
@@ -1,9 +1,19 @@
 import makeRegisterUser from './register-user'
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 import { usersDb } from '../../data-access'
 
-//TODO: use bcrypt
+const PRIVATE_KEY = process.env.PRIVATE_KEY
 const hash = pass => bcrypt.hash(pass, 10)
+//TODO: handle expiration date
+const generateToken = user => jwt.sign(user, PRIVATE_KEY)
+const validateToken = token => {
+  try {
+    return jwt.verify(token, PRIVATE_KEY)
+  } catch (e) {
+    return null
+  }
+}
 
 const registerUser = makeRegisterUser({ userDb: usersDb, hash})
 
